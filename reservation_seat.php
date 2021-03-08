b1bcddc3f31c13858ee8610224b992db07ae0994
SEIM <68439871+SEIM-CHAN@users.noreply.github.com>
2021-03-08 10:15:16 +0900

    初期ファイル設定

---
+++ reservation_seat.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    
+<input type="hidden" id="seigen_su" name="seigen_su" value="1">
+    <div class="sub-section indent01 separate">
+        <h5 class="normal-header large-font">座席指定</h5>
+        <div class="box02 pad20 clearfix">
+
+            <div class="box left w512 pad10">
+                <div id="seat-select-screen">スクリーン</div>
+                <table summary="座席指定" id="seat-select-table" class="seat-table">
+                    <tr>
+                        <th abbr="A列">A列</th>
+                        <td>
+                            A列<br />1番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106557_A_1"
+                                name="seatNum_10015106557_A_1" id="seatNum_A_01" value="A列 1番" />
+                        </td>
+                        <td>
+                            A列<br />2番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106558_A_2"
+                                name="seatNum_10015106558_A_2" id="seatNum_A_01" value="A列 2番" />
+                        </td>
+                        <td>
+                            A列<br />3番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106559_A_3"
+                                name="seatNum_10015106559_A_3" id="seatNum_A_01" value="A列 3番" />
+                        </td>
+                        <td>
+                            A列<br />4番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106560_A_4"
+                                name="seatNum_10015106560_A_4" id="seatNum_A_01" value="A列 4番" />
+                        </td>
+                        <td>
+                            A列<br />5番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106561_A_5"
+                                name="seatNum_10015106561_A_5" id="seatNum_A_01" value="A列 5番" />
+                        </td>
+                        <td>
+                            A列<br />6番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106562_A_6"
+                                name="seatNum_10015106562_A_6" id="seatNum_A_01" value="A列 6番" />
+                        </td>
+                        <td>
+                            A列<br />7番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106563_A_7"
+                                name="seatNum_10015106563_A_7" id="seatNum_A_01" value="A列 7番" />
+                        </td>
+                        <td>
+                            A列<br />8番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106564_A_8"
+                                name="seatNum_10015106564_A_8" id="seatNum_A_01" value="A列 8番" />
+                        </td>
+                        <td>
+                            A列<br />9番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106565_A_9"
+                                name="seatNum_10015106565_A_9" id="seatNum_A_01" value="A列 9番" />
+                        </td>
+                        <td>
+                            A列<br />10番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106566_A_10"
+                                name="seatNum_10015106566_A_10" id="seatNum_A_01" value="A列 10番" />
+                        </td>
+                        <td>
+                            A列<br />11番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106567_A_11"
+                                name="seatNum_10015106567_A_11" id="seatNum_A_01" value="A列 11番" />
+                        </td>
+                        <td>
+                            A列<br />12番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106568_A_12"
+                                name="seatNum_10015106568_A_12" id="seatNum_A_01" value="A列 12番" />
+                        </td>
+                    <tr>
+                        <th abbr="B列">B列</th>
+                        <td>
+                            B列<br />1番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106571_B_1"
+                                name="seatNum_10015106571_B_1" id="seatNum_A_01" value="B列 1番" />
+                        </td>
+                        <td>
+                            B列<br />2番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106572_B_2"
+                                name="seatNum_10015106572_B_2" id="seatNum_A_01" value="B列 2番" />
+                        </td>
+                        <td>
+                            B列<br />3番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106573_B_3"
+                                name="seatNum_10015106573_B_3" id="seatNum_A_01" value="B列 3番" />
+                        </td>
+                        <td>
+                            B列<br />4番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106574_B_4"
+                                name="seatNum_10015106574_B_4" id="seatNum_A_01" value="B列 4番" />
+                        </td>
+                        <td>
+                            B列<br />5番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106575_B_5"
+                                name="seatNum_10015106575_B_5" id="seatNum_A_01" value="B列 5番" />
+                        </td>
+                        <td>
+                            B列<br />6番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106576_B_6"
+                                name="seatNum_10015106576_B_6" id="seatNum_A_01" value="B列 6番" />
+                        </td>
+                        <td>
+                            B列<br />7番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106577_B_7"
+                                name="seatNum_10015106577_B_7" id="seatNum_A_01" value="B列 7番" />
+                        </td>
+                        <td>
+                            B列<br />8番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106578_B_8"
+                                name="seatNum_10015106578_B_8" id="seatNum_A_01" value="B列 8番" />
+                        </td>
+                        <td>
+                            B列<br />9番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106579_B_9"
+                                name="seatNum_10015106579_B_9" id="seatNum_A_01" value="B列 9番" />
+                        </td>
+                        <td>
+                            B列<br />10番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106580_B_10"
+                                name="seatNum_10015106580_B_10" id="seatNum_A_01" value="B列 10番" />
+                        </td>
+                        <td>
+                            B列<br />11番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106581_B_11"
+                                name="seatNum_10015106581_B_11" id="seatNum_A_01" value="B列 11番" />
+                        </td>
+                        <td>
+                            B列<br />12番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106582_B_12"
+                                name="seatNum_10015106582_B_12" id="seatNum_A_01" value="B列 12番" />
+                        </td>
+                    <tr>
+                        <th abbr="C列">C列</th>
+                        <td>
+                            C列<br />1番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106585_C_1"
+                                name="seatNum_10015106585_C_1" id="seatNum_A_01" value="C列 1番" />
+                        </td>
+                        <td>
+                            C列<br />2番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106586_C_2"
+                                name="seatNum_10015106586_C_2" id="seatNum_A_01" value="C列 2番" />
+                        </td>
+                        <td>
+                            C列<br />3番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106587_C_3"
+                                name="seatNum_10015106587_C_3" id="seatNum_A_01" value="C列 3番" />
+                        </td>
+                        <td>
+                            C列<br />4番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106588_C_4"
+                                name="seatNum_10015106588_C_4" id="seatNum_A_01" value="C列 4番" />
+                        </td>
+                        <td>
+                            C列<br />5番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106589_C_5"
+                                name="seatNum_10015106589_C_5" id="seatNum_A_01" value="C列 5番" />
+                        </td>
+                        <td>
+                            C列<br />6番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106590_C_6"
+                                name="seatNum_10015106590_C_6" id="seatNum_A_01" value="C列 6番" />
+                        </td>
+                        <td>
+                            C列<br />7番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106591_C_7"
+                                name="seatNum_10015106591_C_7" id="seatNum_A_01" value="C列 7番" />
+                        </td>
+                        <td>
+                            C列<br />8番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106592_C_8"
+                                name="seatNum_10015106592_C_8" id="seatNum_A_01" value="C列 8番" />
+                        </td>
+                        <td>
+                            C列<br />9番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106593_C_9"
+                                name="seatNum_10015106593_C_9" id="seatNum_A_01" value="C列 9番" />
+                        </td>
+                        <td>
+                            C列<br />10番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106594_C_10"
+                                name="seatNum_10015106594_C_10" id="seatNum_A_01" value="C列 10番" />
+                        </td>
+                        <td>
+                            C列<br />11番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106595_C_11"
+                                name="seatNum_10015106595_C_11" id="seatNum_A_01" value="C列 11番" />
+                        </td>
+                        <td>
+                            C列<br />12番
+                            <input type="checkbox" class="seatNumInput" id="seatNum_10015106596_C_12"
+                                name="seatNum_10015106596_C_12" id="seatNum_A_01" value="C列 12番" />
+                        </td>
+                    <tr>
+                        
+                        <td style="visibility:hidden"></td>
+                </table>
+                <div id="seat-select-door">出入口</div>
+                <div id="seat-select-door" class="right">出入口</div>
+                <p class="red mt5 mb0 pl40"><img src="../images/seat_select/check_out.gif" alt="" width="13" height="13"
+                        class="inline-img" /> オンラインでの販売はございません。<br />
+                    <img src="../images/seat_select/check_ok.gif" alt="" width="13" height="13" class="inline-img" />
+                    ご購入可能席です。</p>
+
+</body>
+
+</html>
