43f1b216e9b0d143e0e73d7b72348fd45e6fee5a
SEIM <68439871+SEIM-CHAN@users.noreply.github.com>
2021-03-09 11:34:33 +0900

    型制作途中

--- index.php
+++ index.php
@@ -1,1 +1,46 @@
-<?php phpinfo(); ?>
+<!DOCTYPE HTML>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>大原四季</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <div class="headerTable">
+                <div class="header_left">
+                    <div class="table">
+                        <h1 class="logo">
+                            <a href="/"><img src="images/logo/大原四季.001.png" alt="劇団四季" class="logo_ohara"></a>
+                        </h1>
+                    </div>
+                </div>
+                <div class="header_right">
+                    <ul class="nav navType1 floatLeft">
+                        <li><a href="/tickets/" class="icon icon_tickets">チケット予約</a></li>
+                        <li class="spBlock idLogin"><a href="javascript:void(0);" class="icon icon_login">ログイン</a></li>
+                    </ul>
+                </div>
+            </div><!-- /header -->
+            <div id="navi">
+                　
+            </div><!-- /navi -->
+            <div id="content">
+                <div id="menu">
+                    　
+                </div><!-- /menu -->
+                <div id="main">
+                    　
+                </div><!-- /main -->
+            </div><!-- /content -->
+            <div id="footer">
+                　
+            </div><!-- /footer -->
+        </div><!-- /wrapper -->
+</body>
+
+</html>
