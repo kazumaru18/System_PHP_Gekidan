96427ba439f447c908d36f37fcbe1f2f1e0ccf9d
kazumaru18 <k.tobe015@gmail.com>
2021-03-09 12:45:15 +0900

    login追加

--- login.php
+++ login.php
@@ -1,29 +1,4 @@
 <?php session_start(); ?>
-<?php
-	//customerセッション変数を破棄
-	unset($_SESSION['customer']);
-	//MySQLデータベースに接続する
-	require 'db_connect.php';
-	//SQL文を作る（プレースホルダを使った式）
-	$sql = "select * from user where login = :login and password = :password";
-	//プリペアードステートメントを作る
-	$stm = $pdo->prepare($sql);
-	//プリペアードステートメントに値をバインドする
-	$stm->bindValue(':login',$_POST['login'],PDO::PARAM_STR);
-	$stm->bindValue(':password',$_POST['password'],PDO::PARAM_STR);
-	//SQL文を実行する
-	$stm->execute();
-	//結果の取得（連想配列で受け取る）
-	$result = $stm->fetchAll(PDO::FETCH_ASSOC);
-	//customerセッションの設定
-	foreach ($result as $row) {
-		$_SESSION['customer'] = [
-			'id' => $row['id'], 'name' => $row['name'],
-			'address' => $row['address'], 'login' => $row['login'],
-			'password' => $row['password']
-		];
-	}
-?>
 <!DOCTYPE html>
 <html>
 
@@ -39,11 +14,11 @@ if (!(isset($_SESSION['user']))) {
     //customerセッション変数を破棄
 	unset($_SESSION['user']);
 	//MySQLデータベースに接続する
-	 // データベースユーザ
+	// データベースユーザ
     $user = 'shop';
     $password = 'shoppass';
     // 利用するデータベース
-    $dbName = 'shop';
+    $dbName = 'gekidan';
     // MySQLサーバ
     $host = 'localhost';
     // MySQLのDSN文字列
