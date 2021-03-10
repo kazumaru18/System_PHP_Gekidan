3a79be58dbc73946706f194403097bdcb6d5bd37
utsugi <k.utsugi021@gmail.com>
2021-03-10 12:29:51 +0900

    今日分

--- index.php
+++ index.php
@@ -1,1 +1,49 @@
-<?php phpinfo(); ?>
+<head>
+    <meta charset="utf-8">
+    <title>大原四季</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <nav>
+                <input type="checkbox" id="check">
+                <label for="check" class="checkbtn">
+                    <i class="fas fa-bars"></i>
+                </label>
+                <a href="" class="logo"><img src="images/logo/大原四季.001.png" class="ohara-logo" alt=""></a>
+                <ul>
+                    <li><a href="#"><img src="images/logo/bag.svg" class="ticket_logo" alt=""></a></li>
+                    <li><a href="#"><img src="images/logo/person-check.svg" class="login" alt=""></a></li>
+                </ul>
+            </nav>
+            <section></section>
+        </div><!-- /header -->
+        <div id="navi">
+            　
+        </div><!-- /navi -->
+        <div id="content">
+            <div id="menu">
+                　
+            </div><!-- /menu -->
+            <div id="main">
+                　
+            </div><!-- /main -->
+        </div><!-- /content -->
+        <div id="footer">
+            <div class="logo">
+                <a href="">
+                    <img src="img/logo.svg" alt="">
+                </a>
+            </div>
+            <ul class="nav">
+                <li><a href="https://www.shiki.jp/">source</a></li>
+            </ul>
+            <p class="copyright">
+                COPYRIGHT © O-hara class 1-11. All rights Reserved.
+            </p>
+        </div><!-- /footer -->
+    </div><!-- /wrapper -->
+</body>
+</html>
