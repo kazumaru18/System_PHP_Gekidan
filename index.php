77f27368071ed6c954f36855d639015197265fe9
SEIM <68439871+SEIM-CHAN@users.noreply.github.com>
2021-03-10 10:55:57 +0900

    header分割

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE HTML>
 <html lang="ja">
 
@@ -11,17 +12,9 @@
 <body>
     <div id="wrapper">
         <div id="header">
-            <nav>
-                <input type="checkbox" id="check">
-                <label for="check" class="checkbtn">
-                    <i class="fas fa-bars"></i>
-                </label>
-                <a href="" class="logo"><img src="images/logo/大原四季.001.png" class="ohara-logo" alt=""></a>
-                <ul>
-                    <li><a href="#"><img src="images/logo/bag.svg" class="ticket_logo" alt=""></a></li>
-                    <li><a href="#"><img src="images/logo/person-check.svg" class="login" alt=""></a></li>
-                </ul>
-            </nav>
+            <?php 
+            require_once('header.php');
+            ?>
             <section></section>
         </div><!-- /header -->
         <div id="navi">
