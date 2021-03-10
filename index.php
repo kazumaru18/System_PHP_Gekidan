6431cd5eaf8051f01d9b0f0fbbb2bf90a3ece087
SEIM <68439871+SEIM-CHAN@users.noreply.github.com>
2021-03-10 10:00:09 +0900

    index雛形

--- index.php
+++ index.php
@@ -11,38 +11,44 @@
 <body>
     <div id="wrapper">
         <div id="header">
-            <div class="headerTable">
-                <div class="header_left">
-                    <div class="table">
-                        <h1 class="logo">
-                            <a href="/"><img src="images/logo/大原四季.001.png" alt="劇団四季" class="logo_ohara"></a>
-                        </h1>
-                    </div>
-                </div>
-                <div class="header_right">
-                    <div class="icon_table01">
-                        <a href="" class="icon icon_tickets"><img src="images/logo/ticket.png" alt="チケット" class="ticket_logo"></a>
-                    </div>
-                    <div class="icon_table02">
-                        <a href="" class="icon icon_login"><img src="images/logo/ダウンロード.png" alt="ログイン" class="login_logo"></a>
-                    </div>
-                </div>
-            </div><!-- /header -->
-            <div id="navi">
+            <nav>
+                <input type="checkbox" id="check">
+                <label for="check" class="checkbtn">
+                    <i class="fas fa-bars"></i>
+                </label>
+                <a href="" class="logo"><img src="images/logo/大原四季.001.png" class="ohara-logo" alt=""></a>
+                <ul>
+                    <li><a href="#"><img src="images/logo/bag.svg" class="ticket_logo" alt=""></a></li>
+                    <li><a href="#"><img src="images/logo/person-check.svg" class="login" alt=""></a></li>
+                </ul>
+            </nav>
+            <section></section>
+        </div><!-- /header -->
+        <div id="navi">
+            　
+        </div><!-- /navi -->
+        <div id="content">
+            <div id="menu">
                 　
-            </div><!-- /navi -->
-            <div id="content">
-                <div id="menu">
-                    　
-                </div><!-- /menu -->
-                <div id="main">
-                    　
-                </div><!-- /main -->
-            </div><!-- /content -->
-            <div id="footer">
+            </div><!-- /menu -->
+            <div id="main">
                 　
-            </div><!-- /footer -->
-        </div><!-- /wrapper -->
+            </div><!-- /main -->
+        </div><!-- /content -->
+        <div id="footer">
+            <div class="logo">
+                <a href="">
+                    <img src="img/logo.svg" alt="">
+                </a>
+            </div>
+            <ul class="nav">
+                <li><a href="https://www.shiki.jp/">source</a></li>
+            </ul>
+            <p class="copyright">
+                COPYRIGHT © O-hara class 1-11. All rights Reserved.
+            </p>
+        </div><!-- /footer -->
+    </div><!-- /wrapper -->
 </body>
 
 </html>
