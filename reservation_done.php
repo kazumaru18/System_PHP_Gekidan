265d1b1df80c558c5c5c1afa1f04e39cfa060b32
utsugi <k.utsugi021@gmail.com>
2021-03-12 12:13:26 +0900

    今日分

---
+++ reservation_done.php
@@ -0,0 +1,53 @@
+<head>
+    <meta charset="utf-8">
+    <title>大原四季</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div id="wrapper">
+        <div id="header">
+            <nav>
+                <input type="checkbox" id="check">
+                <label for="check" class="checkbtn">
+                    <i class="fas fa-bars"></i>
+                </label>
+                <a href="" class="logo"><img src="images/logo/大原四季.001.png" class="ohara-logo" alt=""></a>
+                <ul>
+                    <li><a href="#"><img src="images/logo/bag.svg" class="ticket_logo" alt=""></a></li>
+                    <li><a href="#"><img src="images/logo/person-check.svg" class="login" alt=""></a></li>
+                </ul>
+            </nav>
+            
+        </div><!-- /header -->
+        <div id="main" class="main_revdone">
+            <h2>予約が完了しました</h2>
+            <img src="images/087048.png" class="finish_img">
+        </div>
+        <div id="button">
+            <a href="index.php"  class="home_button">ホームに戻る</a>
+        </div>
+        <footer>
+        <div id="footer">
+            <div class="logo">
+                <a href="">
+                    <img src="img/logo.svg" alt="">
+                </a>
+            </div>
+            
+            <p class="copyright">
+                COPYRIGHT © O-hara class 1-11. All rights Reserved.
+            </p>
+            <style type="text/css">
+                #footer{
+                    width: 100%;
+                    position: absolute;/*←絶対位置*/
+                    bottom: 0; /*下に固定*/
+                }
+                </style>
+            
+        </div>
+        </footer><!-- /footer -->
+    </div><!-- /wrapper -->
+</body>
+</html>
